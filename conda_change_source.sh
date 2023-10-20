c699685ce0d28c119fc799fd550701dbb54219db
Ge Haizhou <72548967+OpenGHz@users.noreply.github.com>
2023-10-20 21:09:07 +0800

    add conda change source sh

---
+++ conda_change_source.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+[ "$1" = 'check' ] && conda config --show channels && exit
+
+[ "$1" = 'restore' ] && conda config --remove-key channels && exit
+
+[ "$1" != "" ] && echo "Usage: $1 [check]" && exit
+
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/msys2/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/bioconda/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/menpo/
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch/
+conda config --set show_channel_urls yes
