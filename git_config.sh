6136325ad45bdd09b7a9db185e21766c8528e57a
OpenGHz <1352674740@qq.com>
2024-01-20 18:29:17 +0800

    init

---
+++ git_config.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# please modify the name and e-mail to yours
+git config --global user.name OpenGHz
+git config --global user.email 1352674740@qq.com
+# git config --global user.password "123456"
+
+# show the configs
+git config user.name
+git config user.email
+# git config user.password
+
+# https://zhuanlan.zhihu.com/p/120862483
+
+# git config --global --unset user.name
+# git config --global --unset user.email
+# git config --global --unset user.password
