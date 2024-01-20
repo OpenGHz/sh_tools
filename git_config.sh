aaae2d49b058c700fefe4efaae9fec64a22c7821
OpenGHz <1352674740@qq.com>
2024-01-20 18:52:30 +0800

    add more annotation

--- git_config.sh
+++ git_config.sh
@@ -15,3 +15,10 @@ git config user.email
 # git config --global --unset user.name
 # git config --global --unset user.email
 # git config --global --unset user.password
+
+# 在我们首次使用Git时，都是需要配置用户名(user.name)和邮箱(user.email)的。对于用户名和邮箱来说，有3个地方可以设置
+
+# /etc/gitconfig，使用git config --system，这个是针对整个系统生效的，几乎不会使用
+# ~/.gitconfig,使用 git config -- global,这个是针对用户的，对系统中这个用户的所有项目都生效，很常用
+# 在项目的.git/config中设置，使用git config -- local,这是针对某个项目设置用户名和邮箱的
+# 我们可以使用git config查看配置帮助
