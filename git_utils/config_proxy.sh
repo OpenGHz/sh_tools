fa995aaa9eb9d6479c6480ffd1dcee849f5dc4af
OpenGHz <1352674740@qq.com>
2024-04-19 14:08:18 +0800

    refector

--- git_utils/config_proxy.sh
+++ git_utils/config_proxy.sh
@@ -6,12 +6,14 @@ if [ $1 = "unset" ]; then
     git config --global --unset http.proxy
     git config --global --unset https.proxy
     exit 0
+elif [ $1 = "get" ]; then
+    git config --global --get http.proxy
+    git config --global --get https.proxy
+    exit 0
+elif [ $1 = "set" ]; then
+    git config --global http.proxy 127.0.0.1:7890
+    git config --global https.proxy 127.0.0.1:7890
 fi
-
-# config the proxy
-# please modify the proxy to yours
-git config --global http.proxy 127.0.0.1:7890
-git config --global https.proxy 127.0.0.1:7890
 
 # show the configs
 git config http.proxy
