ce8269e89fa3cd4e58ec94c979c3dfbc7635123e
Haizhou Ge <1352674740@qq.com>
2025-03-06 21:21:27 +0800

    remove bioconda

--- python_utils/conda_change_source.sh
+++ python_utils/conda_change_source.sh
@@ -20,7 +20,8 @@ conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/main/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/conda-forge/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/msys2/
-conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/bioconda/
+# conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/bioconda/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/menpo/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/cloud/pytorch/
+
 conda config --set show_channel_urls yes
